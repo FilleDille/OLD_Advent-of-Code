23e6167ab9af6fddc73c80ba670f38e49f9f012e
FilleDille <filipengstrom91@gmail.com>
2022-12-04 07:15:13 +0100

    Moved a majority of part 1 solution to class to make code cleaner by not repeating in part 2, at the cost of one extra iteration. Both part 1 and 2 takes about 11,3 ms to run

--- 2022/4/4.py
+++ 2022/4/4.py
@@ -9,28 +9,35 @@ class elf:
         raw = f.read().splitlines()
         f.close()
 
+    counter = 0
+    elf_1_sets = []
+    elf_2_sets = []
+
+    for line in raw:
+        elf_1_raw = line.split(',')[0]
+        n1_1 = elf_1_raw.split('-')[0]
+        n2_1 = elf_1_raw.split('-')[1]
+
+        elf_2_raw = line.split(',')[1]
+        n1_2 = elf_2_raw.split('-')[0]
+        n2_2 = elf_2_raw.split('-')[1]
+
+        elf_1_sets.append(set(range(int(n1_1), int(n2_1) + 1)))
+        elf_2_sets.append(set(range(int(n1_2), int(n2_2) + 1)))
+
     def part_1():
-        counter = 0
-
-        for line in elf.raw:
-            elf_1_raw = line.split(',')[0]
-            n1_1 = elf_1_raw.split('-')[0]
-            n2_1 = elf_1_raw.split('-')[1]
-
-            elf_2_raw = line.split(',')[1]
-            n1_2 = elf_2_raw.split('-')[0]
-            n2_2 = elf_2_raw.split('-')[1]
-
-            elf_1 = set(range(int(n1_1), int(n2_1) + 1))
-            elf_2 = set(range(int(n1_2), int(n2_2) + 1))
-
+        for elf_1, elf_2 in zip(elf.elf_1_sets, elf.elf_2_sets):
             if len(elf_1 - elf_2) == 0 or len(elf_2 - elf_1) == 0:
-                counter += 1
+                elf.counter += 1
 
-        return counter
+        return elf.counter
 
     def part_2():
-        pass
+        for elf_1, elf_2 in zip(elf.elf_1_sets, elf.elf_2_sets):
+            if len(elf_1 & elf_2) > 0:
+                elf.counter += 1
+
+        return elf.counter
 
 
 if __name__ == "__main__":
