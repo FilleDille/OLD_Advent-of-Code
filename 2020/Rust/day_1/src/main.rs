29ce04df089c2e170c3c3c9587bb1ee689729f80
filipengstrom <filipengstrom91@gmail.com>
2023-06-16 07:10:51 +0200

    Day 1 part 2 Rust

--- 2020/Rust/day_1/src/main.rs
+++ 2020/Rust/day_1/src/main.rs
@@ -1,32 +1,53 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::time::Instant;
 
 fn main() -> std::io::Result<()> {
-    let t0 = Instant::now();
-
     let mut file = File::open("input.txt")?;
     let mut contents = String::new();
 
     file.read_to_string(&mut contents)?;
 
     let lines_str: Vec<&str> = contents.split('\n').collect();
-    let lines_int: Vec<i32> = lines_str.iter()
+    let lines_u: Vec<u32> = lines_str.iter()
         .filter(|&&line| !line.is_empty())
-        .map(|line| line.parse::<i32>().unwrap())
+        .map(|line| line.parse::<u32>().unwrap())
         .collect();
 
-    for number in &lines_int {
-        let missing_number = 2020 - number;
-        if lines_int.contains(&missing_number) {
-            let prod = number * missing_number;
-            println!("{number} * {missing_number} = {prod}");
-            break;
-        }
-    }
-
-    let duration = t0.elapsed();
-    println!("Elapsed time: {:?}", duration);
+    part_2(&lines_u);
 
     Ok(())
+}
+
+#[allow(dead_code)]
+fn part_1(lines_u: &Vec<u32>) {
+    for number in lines_u {
+        let missing_number_1 = 2020 - number;
+
+        if lines_u.contains(&missing_number_1) {
+            let prod = number * missing_number_1;
+            println!("{number} * {missing_number_1} = {prod}");
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn part_2(lines_u: &Vec<u32>) {
+    'first_loop: for number_1 in lines_u {
+        let missing_number_1 = 2020 - number_1;
+
+        let lines_u_2: Vec<u32> = lines_u.iter()
+            .filter(|&nmbr| *nmbr <= missing_number_1)
+            .copied()
+            .collect();
+
+        for number_2 in &lines_u_2 {
+            let missing_number_2 = missing_number_1 - number_2;
+
+            if lines_u_2.contains(&missing_number_2) {
+                let prod = number_1 * number_2 * missing_number_2;
+                println!("{number_1} * {number_2} * {missing_number_2} = {prod}");
+                break 'first_loop;
+            }
+        }
+    }
 }
