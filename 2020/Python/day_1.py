71e657de1ef4374bba28a44e4186366a96b9e23c
filipengstrom <filipengstrom91@gmail.com>
2023-06-16 07:36:17 +0200

    Finished day 1 part 2 for python part

--- 2020/Python/day_1.py
+++ 2020/Python/day_1.py
@@ -1,17 +1,29 @@
-import timeit
+def main():
+    with open("input.txt", 'r') as f:
+        raw = list(map(lambda x: int(x.replace('\n', '')), f.readlines()))
+    part_2(raw)
 
-t0 = timeit.default_timer()
+def part_1(inp):
+    for number in inp:
+        missing_number = 2020 - number
+        if missing_number in inp:
+            prod = number * missing_number
+            print(f'{number} * {missing_number} = {prod}')
+            break
 
-with open("input.txt", 'r') as f:
-    raw = list(map(lambda x: int(x.replace('\n', '')), f.readlines()))
+def part_2(inp):
+    for number_1 in inp:
+        missing_number_1 = 2020 - number_1
+        raw_filtered = list(filter(lambda x: x <= missing_number_1, inp))
 
-for number in raw:
-    missing_number = 2020 - number
-    if missing_number in raw:
-        prod = number * missing_number
-        print(f'{number} * {missing_number} = {prod}')
+        for number_2 in raw_filtered:
+            missing_number_2 = missing_number_1 - number_2
+
+            if missing_number_2 in raw_filtered:
+                prod = number_1 * number_2 * missing_number_2
+                print(f'{number_1} * {number_2} * {missing_number_2} = {prod}')
+                break
+        else:
+            continue
         break
-
-t1 = timeit.default_timer()
-
-print(f'Elapsed time: {round((t1-t0) * 1_000_000, 3)} µs')
+main()
