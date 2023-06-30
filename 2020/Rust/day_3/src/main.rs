93b5920d589791e7b33bf7554342547ef977a1d3
filipengstrom <filipengstrom91@gmail.com>
2023-06-30 14:14:01 +0200

    Added part 2

--- 2020/Rust/day_3/src/main.rs
+++ 2020/Rust/day_3/src/main.rs
@@ -1,6 +1,11 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
+struct Directions {
+    right: usize,
+    down: usize,
+}
+
 fn count_trees(map: &[Vec<char>], right: usize, down: usize) -> usize {
     let mut tree_count = 0;
     let mut x = 0;
@@ -22,8 +27,10 @@ fn count_trees(map: &[Vec<char>], right: usize, down: usize) -> usize {
 fn main() -> io::Result<()> {
     let file = File::open("input_3.txt")?;
     let reader = io::BufReader::new(file);
-
+    let mut dir_vec: Vec<Directions> = Vec::new();
+    let mut hit_vec: Vec<usize> = Vec::new();
     let mut map: Vec<Vec<char>> = Vec::new();
+    let tree_prod: usize;
 
     for line in reader.lines() {
         let line = line?;
@@ -31,8 +38,19 @@ fn main() -> io::Result<()> {
         map.push(chars);
     }
 
-    let tree_count = count_trees(&map, 3, 1);
-    println!("Encountered trees: {}", tree_count);
+    dir_vec.push(Directions{right: 1, down: 1});
+    dir_vec.push(Directions{right: 3, down: 1});
+    dir_vec.push(Directions{right: 5, down: 1});
+    dir_vec.push(Directions{right: 7, down: 1});
+    dir_vec.push(Directions{right: 1, down: 2});
+
+    for dir in dir_vec {
+        hit_vec.push(count_trees(&map, dir.right, dir.down));
+    }
+
+    tree_prod = hit_vec.iter().product();
+
+    println!("Encountered trees: {}", tree_prod);
 
     Ok(())
 }
