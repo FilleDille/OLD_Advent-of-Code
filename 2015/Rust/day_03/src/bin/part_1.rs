aab9a7bc09498d6f5b40d53ca7c5506786993f69
filipengstrom <filipengstrom91@gmail.com>
2023-11-06 19:30:39 +0100

    Forgot to commit last time

---
+++ 2015/Rust/day_03/src/bin/part_1.rs
@@ -0,0 +1,5 @@
+use day_03::process_part_1;
+
+fn main() {
+    println!("{}", process_part_1());
+}
---
+++ 2015/Rust/day_03/src/bin/part_2.rs
@@ -0,0 +1,5 @@
+use day_03::process_part_2;
+
+fn main() {
+    println!("{}", process_part_2());
+}
